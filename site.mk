c9693ac65abef319dc7e48f5ce8409ec6256e9cd
Freifunk Westpfalz <ffwp.github@schmittendrin.de>
2014-10-16 00:15:34 +0200

    ntp servers ipv6, autoupdater probability reintroduced and priority changed, tc limits doubled, mtu 1406, hour and minute in images name

--- site.mk
+++ site.mk
@@ -19,8 +19,8 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.1+0-beta$(shell date '+%Y%m%d')
-DEFAULT_GLUON_PRIORITY := 0
+DEFAULT_GLUON_RELEASE := 0.1+0-beta$(shell date '+%Y%m%d-%H%M')
+DEFAULT_GLUON_PRIORITY := 0.1
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
