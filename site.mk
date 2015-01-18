c20e3638985fd5a2b42a7c11b9e74008d1e77bb6
Freifunk Westpfalz <ffwp.github@schmittendrin.de>
2015-01-18 22:08:09 +0100

    Changes for 2014.4

--- site.mk
+++ site.mk
@@ -1,17 +1,23 @@
 GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
 	gluon-announce \
 	gluon-announced \
 	gluon-autoupdater \
-	gluon-config-mode \
 	gluon-cron \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-neighbour-info \
 	gluon-next-node \
-	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
@@ -19,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.1.4-$(shell date '+%y%m%d-%H%M')
+DEFAULT_GLUON_RELEASE := 0.2.0-$(shell date '+%y%m%d-%H%M')
 DEFAULT_GLUON_PRIORITY := 0.1
 
 # Allow overriding the release number from the command line
