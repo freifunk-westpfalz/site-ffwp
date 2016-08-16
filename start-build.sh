c0916ea41a127fdcad3c0415dd82cdfc4aae218e
Little-Ben <Little-Ben@users.noreply.github.com>
2016-08-16 17:26:20 +0200

    only generate sha files if not present

--- start-build.sh
+++ start-build.sh
@@ -14,7 +14,7 @@ if [ ! -d "site" ]; then
 fi
 cd site 
 
-PATH_LOG=../../.ffwp 
+PATH_LOG=../../.ffwp
 PATH_GLUON=../../gluon
 FILE_SITE=$PATH_LOG/site.sha
 FILE_GLUON=$PATH_LOG/gluon.sha
@@ -22,10 +22,21 @@ FILE_SITE_OLD=$PATH_LOG/site.old.sha
 FILE_GLUON_OLD=$PATH_LOG/gluon.old.sha
 FILE_LOG=$PATH_LOG/start-build.log
 
-touch $FILE_SITE
-touch $FILE_SITE_OLD
-touch $FILE_GLUON
-touch $FILE_GLUON_OLD
+if [ ! -f "$FILE_SITE_OLD" ]; then 
+	echo "1" > $FILE_SITE_OLD
+fi
+
+if [ ! -f "$FILE_GLUON_OLD" ]; then 
+	echo "1" > $FILE_GLUON_OLD
+fi
+
+if [ ! -f "$FILE_SITE" ]; then 
+	echo "0" > $FILE_SITE
+fi
+
+if [ ! -f "$FILE_GLUON" ]; then 
+	echo "0" > $FILE_GLUON
+fi
 
 echo "Repositories aktualisieren..."
 ./pull-repos.sh
